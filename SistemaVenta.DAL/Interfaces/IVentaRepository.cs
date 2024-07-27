0104af07d774b03f02db9102e47702cf377da9ae
cferomero <cferomero@outlook.com>
2024-07-27 11:54:28 -0500

    Nueba interfaz de Venta

---
+++ SistemaVenta.DAL/Interfaces/IVentaRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SistemaVenta.Entity;
+
+namespace SistemaVenta.DAL.Interfaces
+{
+    public interface IVentaRepository : IGenericRepository<Venta>
+    {
+        //16
+    }
+}
